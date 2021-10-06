cc331da47200e0e4478d3cf6284acd72f906e545
Willames Santos <willamessantos@live.com>
2021-10-06 11:35:40 -0300

    destructuring of components

---
+++ client/src/components/footer-admin.js
@@ -0,0 +1,15 @@
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+
+export default function FooterAdmin(props) {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="javascript:;">
+          Curso Básico MERN.
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
